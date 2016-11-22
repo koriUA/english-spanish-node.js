b079956a38703f588e524681d65116e4626d105e
Vitaliy Ivanchyshyn <vivanchyshyn@softjourn.com>
2016-11-22 16:32:04 +0200

    some chages

--- index1.js
+++ index1.js
@@ -1,7 +1,26 @@
 const fs = require('fs');
 const xlsx = require('node-xlsx');
+var deepForEach = require('deep-for-each');
+
 var enUS = require('./en-US.json');
 var esMX = require('./es-MX.json');
+
+
+var cloneEnUS = JSON.parse(JSON.stringify(enUS));
+
+deepForEach(cloneEnUS, function (value, prop, subject, path) {
+	if (value instanceof Object){
+		return;
+	}
+
+	try {
+		eval('cloneEnUS.' + path + ' = esMX.' + path);
+	} catch(e){
+		console.log('error', e);
+	}
+
+});
+
 
 const workSheetsFromFile = xlsx.parse('file.xlsx');
 
@@ -9,14 +28,14 @@ const workSheetsFromFile = xlsx.parse('file.xlsx');
 workSheetsFromFile[0].data.forEach( el => {
 	if (el[2]){
 		var key = el[0].split('$').join('.');
-		eval('esMX.' + key + '=' + JSON.stringify(el[2]) + '');
+		eval('cloneEnUS.' + key + '=' + JSON.stringify(el[2]) + '');
 	}
 });
 
 
 
 
-fs.writeFile('new-es-MX.json', JSON.stringify(esMX, null, 2), function (err) {
+fs.writeFile('new-es-MX.json', JSON.stringify(cloneEnUS, null, 2), function (err) {
 	if (err) {
 		throw 'error opening file: ' + err;
 	}
